af2aa04eb744c5afdaa49fa1a6b801d9f82fd855
crynobone <crynobone@gmail.com>
2015-02-22 12:31:03 +0800

    Ensure default cache driver is set to array #58.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ tests/DefaultConfigurationTest.php
@@ -0,0 +1,24 @@
+<?php namespace Orchestra\Testbench\TestCase;
+
+class DefaultConfigurationTest extends \Orchestra\Testbench\TestCase
+{
+    /**
+     * `cache.default` value is set to array.
+     *
+     * @test
+     */
+    public function testDefaultCacheConfiguration()
+    {
+        $this->assertEquals('array', $this->app['config']['cache.default']);
+    }
+
+    /**
+     * `session.driver` value is set to array.
+     *
+     * @test
+     */
+    public function testDefaultSessionConfiguration()
+    {
+        $this->assertEquals('array', $this->app['config']['session.driver']);
+    }
+}
