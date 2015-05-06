5e7ba346a0b9cf14d5c50cca5034588ac0d6b259
crynobone <crynobone@gmail.com>
2015-05-06 17:55:09 +0800

    Update to laravel/laravel@89e2d8e
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Traits/ApplicationTrait.php
+++ src/Traits/ApplicationTrait.php
@@ -105,6 +105,7 @@ trait ApplicationTrait
         return [
             'Illuminate\Foundation\Providers\ArtisanServiceProvider',
             'Illuminate\Auth\AuthServiceProvider',
+            'Illuminate\Broadcasting\BroadcastServiceProvider',
             'Illuminate\Bus\BusServiceProvider',
             'Illuminate\Cache\CacheServiceProvider',
             'Illuminate\Foundation\Providers\ConsoleSupportServiceProvider',
