eac3ba5a7ec01df19a19b82f52eaa82979e02719
crynobone <crynobone@gmail.com>
2015-12-13 00:31:48 +0800

    Add env.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- fixture/config/app.php
+++ fixture/config/app.php
@@ -2,6 +2,20 @@
 
 return [
 
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Environment
+    |--------------------------------------------------------------------------
+    |
+    | This value determines the "environment" your application is currently
+    | running in. This may determine how you prefer to configure various
+    | services your application utilizes. Set this in your ".env" file.
+    |
+    */
+
+    'env' => env('APP_ENV', 'testing'),
+
     /*
     |--------------------------------------------------------------------------
     | Application Debug Mode
