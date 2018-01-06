73f348c8ee3448267c25ffeae3f65660f53d591a
Mior Muhammad Zaki <crynobone@gmail.com>
2018-01-06 12:00:16 +0800

    Fixes command.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- sync.sh
+++ sync.sh
@@ -5,4 +5,4 @@ echo ">>>> Checkout branch $BRANCH"
 git checkout $BRANCH
 git submodule init
 git submodule foreach git checkout $BRANCH
-git submodule update --remote
+git submodule foreach git pull
