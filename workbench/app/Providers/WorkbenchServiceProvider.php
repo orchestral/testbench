f4055488171f927d697e2e4dcc8d6ff4dc433103
Mior Muhammad Zaki <crynobone@gmail.com>
2023-10-16 09:35:23 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- workbench/app/Providers/WorkbenchServiceProvider.php
+++ workbench/app/Providers/WorkbenchServiceProvider.php
@@ -4,7 +4,7 @@ namespace Workbench\App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
-class TestbenchServiceProvider extends ServiceProvider
+class WorkbenchServiceProvider extends ServiceProvider
 {
     public function register()
     {
