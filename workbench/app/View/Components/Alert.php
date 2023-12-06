626b87ca263a1450f0e18698410de686d92ac8e0
Mior Muhammad Zaki <crynobone@gmail.com>
2023-12-06 11:07:39 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ workbench/app/View/Components/Alert.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Workbench\App\View\Components;
+
+use Illuminate\View\Component;
+use Illuminate\View\View;
+
+class Alert extends Component
+{
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render()
+    {
+        return '<div>Alert Component</div>';
+    }
+}
